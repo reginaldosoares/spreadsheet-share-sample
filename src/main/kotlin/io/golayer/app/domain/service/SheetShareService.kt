13ed7e8ffdb309fecefa39370f5be904174e84b6
reginaldosoares <reginaldolsoares@pm.me>
2019-10-29 02:08:49 +0100

    test

--- src/main/kotlin/io/golayer/app/domain/service/SheetShareService.kt
+++ src/main/kotlin/io/golayer/app/domain/service/SheetShareService.kt
@@ -13,7 +13,7 @@ class SheetShareService(private val repository: SheetShareRepository,
      *  produces async Command share event
      */
     fun process(shareCommand: Command<RequestShare>) {
-        event.producesCommand(shareCommand).also { "222xxxxx" }
+        event.producesCommand(shareCommand).also { "222xxxxxxxx222111xxxx" }
     }
 
     /*
